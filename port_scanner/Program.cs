5f9821f039ff61247d3bbc895d60e50cb213172c
Olga Zhukova <zhukova.o.m@yandex.ru>
2018-03-31 10:47:25 +0500

    Сканер UDP

--- port_scanner/Program.cs
+++ port_scanner/Program.cs
@@ -9,8 +9,8 @@ namespace port_scanner
 	{
 		private static void Main(string[] args)
 		{
-            // IPAddress.Parse("10.97.135.51"),
-            var ips = new List<IPAddress> { IPAddress.Parse("80.83.248.25"), IPAddress.Loopback };
+			// IPAddress.Parse("80.83.248.25"),  IPAddress.Parse("10.97.135.51"),
+			var ips = new List<IPAddress> {IPAddress.Loopback };
 			var left = GetPortAsInput("Left edge:");
 			var right = GetPortAsInput("Right edge:");
 			Console.WriteLine();
--- port_scanner/Scanner.cs
+++ port_scanner/Scanner.cs
@@ -6,42 +6,42 @@ using System.Net.NetworkInformation;
 using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
-using Lying_NTP;
+
 
 namespace port_scanner
 {
-	public class Scanner
-	{
+    public class Scanner
+    {
         readonly string[] names = new[] { "IP", "Port", "Result", "Type" };
         const string Format = "{0,-13} {1,-8} {2, -10} {3, -8}\n";
         public void Scan(List<IPAddress> ips, List<int> ports)
-		{
-			Console.WriteLine(Format, names);
+        {
+            Console.WriteLine(Format, names);
+
             Task.WhenAll(ips.Select(ip => Task.Run(async () => {
                 if (await PingAsync(ip) != IPStatus.Success)
                     return ;
                 await Task.WhenAll(ports.Select(port => CheckPort(ip, port)));
 
-                //foreach (var port in ports)
-                //    await CheckPort(ip, port);
-                
             }))).Wait();
         }
 
         public async Task CheckPort(IPAddress ip, int port)
         {
             var tcpPortStatus = await CheckTcpPortAsync(ip, port);
-            //if (tcpPortStatus != PortStatus.Open)
-            //{
-            //    var udpPortStatus = await ChechUdpPortAsync(ip, port);
-            //    Console.WriteLine(Format, ip, port, udpPortStatus, "UDP");
-            //}
-            //else
+            if (tcpPortStatus != PortStatus.Open)
+            {
+                var udpPortStatus = await ChechUdpPort(ip, port);
+                if (udpPortStatus == PortStatus.Open)
+                    Console.WriteLine(Format, ip, port, udpPortStatus, "UDP");
+                else Console.WriteLine(Format, ip, port, tcpPortStatus, "*");
+            }
+            else
                 Console.WriteLine(Format, ip, port, tcpPortStatus, "TCP");
         }
 
         public async  Task<IPStatus> PingAsync(IPAddress ip)
-		{
+        {
             using (var ping = new Ping())
             {
                 var status = await ping.SendPingAsync(ip, 3000);
@@ -49,72 +49,51 @@ namespace port_scanner
             }
         }
 
-        public  PortStatus ChechUdpPort(IPAddress ip, int port)
+        public async Task<PortStatus> ChechUdpPort(IPAddress ip, int port)
         {
             var data = new byte[8];
             var ipEndPoint = new IPEndPoint(ip, port);
 
-            using (var socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp))
+            using (var udpClient = new UdpClient())
             {
-                socket.Connect(ipEndPoint);
-                socket.ReceiveTimeout = 6000;
-                socket.SendTimeout = 2000;
-                
                 try
                 {
-                    socket.Send(data);
-                    socket.Receive(data);
+                    udpClient.Connect(ipEndPoint);
+                    udpClient.Client.ReceiveTimeout = 3000;
+                    udpClient.Send(data, data.Length);
+                   
+                    var receiveTask = udpClient.ReceiveAsync();
+                    if (await Task.WhenAny(receiveTask, Task.Delay(3000)) == receiveTask)
+                    {
+                        Console.WriteLine(Encoding.ASCII.GetString(receiveTask.Result.Buffer));
+                        return PortStatus.Closed;
+                    }
+                    return PortStatus.Open;
                 }
                 catch (Exception e)
                 {
                     return PortStatus.Unknown;
                 }
-
-                var a = 0;
-
-                //switch (recieveTask.Status)
-                //{
-                //    case TaskStatus.RanToCompletion:
-                //        return PortStatus.Open;
-                //    case TaskStatus.Faulted:
-                //        return PortStatus.Closed;
-                //    default:
-                //        return PortStatus.Unknown;
-                //}
-                //socket.ReceiveTimeout = 3000;
-
-                //socket.Send(data);
-                //socket.Receive(data);
-                //socket.Close();
             }
-
-            Console.WriteLine(Encoding.UTF8.GetString(data));
-            return PortStatus.Unknown;
         }
 
-        private byte[] GenerateNtpQuery()
+        public async Task<PortStatus> CheckTcpPortAsync(IPAddress ip, int port)
         {
-            return NtpFrame.GenerateQuery();
-
-        }
-		public async Task<PortStatus> CheckTcpPortAsync(IPAddress ip, int port)
-		{
-			using (var tcpClient = new TcpClient())
-			{
-                //tcpClient.Connect(ip, port);
+            using (var tcpClient = new TcpClient())
+            {
                 var connectTask = tcpClient.ConnectAsync(ip, port);
                 await Task.WhenAny(connectTask, Task.Delay(3000));
 
                 switch (connectTask.Status)
-				{
-					case TaskStatus.RanToCompletion:
-						return PortStatus.Open;
-					case TaskStatus.Faulted:
-						return PortStatus.Closed;
-					default:
-						return PortStatus.Unknown;
-				}
-			}
-		}
-	}
+                {
+                    case TaskStatus.RanToCompletion:
+                        return PortStatus.Open;
+                    case TaskStatus.Faulted:
+                        return PortStatus.Closed;
+                    default:
+                        return PortStatus.Unknown;
+                }
+            }
+        }
+    }
 }
